ba31f65ed3f4ceef283170cf4a96a58c0dc49c15
Darshan Sawardekar <darshan@10up.com>
2022-11-21 19:20:41 +0530

    Adds zip script

---
+++ bin/build-release-zip.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+version=$1
+file=block-catalog-$version.zip
+
+cd ../
+zip -q -r block-catalog/releases/$file block-catalog -x "*/.git/*" "*/bin/*" "*/vendor/*" "*/node_modules/*" "*/releases/*"
+echo $file " Built"
