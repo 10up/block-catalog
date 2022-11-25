f6b021789775abf965c7de6c253192f6e9738004
Darshan Sawardekar <darshan@10up.com>
2022-11-25 11:45:08 +0530

    Adds build-release-svn bash script

---
+++ bin/build-release-svn.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+rsync \
+	-av \
+	--exclude ".git" \
+	--exclude "bin" \
+	--exclude "vendor" \
+	--exclude "node_modules" \
+	--exclude "releases" \
+	--exclude "composer.*" \
+	--exclude ".*" \
+	--exclude "package*.json" \
+	--exclude "phpcs*" \
+	. releases/svn/trunk
