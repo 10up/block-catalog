7029325dd29831e7a141ed0b56287dce630bbf60
Sudip Dadhaniya <61381229+sudip-d@users.noreply.github.com>
2025-08-22 16:10:13 +0530

    Added remaing test cases

--- tests/cypress/e2e/admin.test.js
+++ tests/cypress/e2e/admin.test.js
@@ -22,6 +22,15 @@ describe("Admin can login and open dashboard", () => {
 		cy.get("#submit").click({ force: true });
   });
 
+	it("Verify that you can see the block catalog in post type list view and filter it", () => {
+    cy.visit("/wp-admin");
+		cy.get("#menu-posts").contains('Posts').click();
+		cy.get('.taxonomy-block-catalog.column-taxonomy-block-catalog').contains("Core").should("exist");
+		cy.get('.taxonomy-block-catalog.column-taxonomy-block-catalog').contains("Heading").should("exist");
+		cy.get('select[name="block-catalog"]').select('Heading');
+		cy.get('#post-query-submit').click();
+	});
+
 	it("Ensure that block catalog list should be appeared when you index the posts.", () => {
     cy.visit("/wp-admin");
     cy.get("#menu-posts").contains('Posts').trigger("mouseover").trigger("mouseenter");
@@ -32,6 +41,26 @@ describe("Admin can login and open dashboard", () => {
 		cy.get('.name.column-name.has-row-actions.column-primary').contains("Core").should("exist");
 		cy.get('.name.column-name.has-row-actions.column-primary').contains("Heading").should("exist");
   });
+
+	it("Ensure that you can add new block catalog.", () => {
+    cy.visit("/wp-admin");
+    cy.get("#menu-posts").contains('Posts').trigger("mouseover").trigger("mouseenter");
+		cy.get("#menu-posts .wp-submenu").scrollIntoView({ block: "center" }).should("exist");
+    cy.get("#menu-posts .wp-submenu a").contains("Block Catalog").scrollIntoView({ block: "center" }).click({ force: true });
+		cy.get('#tag-name').type('Quote');
+		cy.get('select[name="parent"]').select('Core');
+		cy.get('#submit').click();
+	});
+
+	it("Make sure indexed post type should be delete when you click on 'Delete Index' Button.", () => {
+    cy.visit("/wp-admin");
+    cy.get("#menu-tools").scrollIntoView({ block: "center" }).trigger("mouseover").trigger("mouseenter");
+    cy.get("#menu-tools .wp-submenu").scrollIntoView({ block: "center" }).should("exist");
+    cy.get("#menu-tools .wp-submenu a").contains("Block Catalog").scrollIntoView({ block: "center" }).click({ force: true });
+    cy.url().should("include", "tools.php?page=block-catalog");
+		cy.get("#delete-index").click({ force: true });
+  });
+
 });
 
 
