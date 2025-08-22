1ee7873a0e235a4954dec24f907c2f011ea4c3c8
Sudip Dadhaniya <61381229+sudip-d@users.noreply.github.com>
2025-08-22 14:06:14 +0530

    add E2E Cypress tests

---
+++ tests/cypress/e2e/admin.test.js
@@ -0,0 +1,37 @@
+describe("Admin can login and open dashboard", () => {
+  beforeEach(() => {
+    cy.login();
+  });
+
+	it('Create post and insert heading', () => {
+    cy.createPost({
+      beforeSave: () => {
+        cy.insertBlock('core/heading').then(id => {
+          cy.getBlockEditor().find(`#${id}`).click().type('This is test heading');
+        });
+      },
+    });
+	});
+
+  it("Ensure that the blocks catalog option is visible in 'Tools' and you can index post types.", () => {
+    cy.visit("/wp-admin");
+    cy.get("#menu-tools").scrollIntoView({ block: "center" }).trigger("mouseover").trigger("mouseenter");
+    cy.get("#menu-tools .wp-submenu").scrollIntoView({ block: "center" }).should("exist");
+    cy.get("#menu-tools .wp-submenu a").contains("Block Catalog").scrollIntoView({ block: "center" }).click({ force: true });
+    cy.url().should("include", "tools.php?page=block-catalog");
+		cy.get("#submit").click({ force: true });
+  });
+
+	it("Ensure that block catalog list should be appeared when you index the posts.", () => {
+    cy.visit("/wp-admin");
+    cy.get("#menu-posts").contains('Posts').trigger("mouseover").trigger("mouseenter");
+		cy.get("#menu-posts .wp-submenu").scrollIntoView({ block: "center" }).should("exist");
+    cy.get("#menu-posts .wp-submenu a").contains("Block Catalog").scrollIntoView({ block: "center" }).click({ force: true });
+		cy.get('#tag-search-input').type('Core');
+		cy.get('#search-submit').click();
+		cy.get('.name.column-name.has-row-actions.column-primary').contains("Core").should("exist");
+		cy.get('.name.column-name.has-row-actions.column-primary').contains("Heading").should("exist");
+  });
+});
+
+
