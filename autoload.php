32ed5d09b87c293e4377865b640fa0cf2e34fff3
Peter Wilson <git-commit-by@peterwilson.cc>
2026-08-13 12:15:14 +1000

    Update name of ignored sniff.

--- autoload.php
+++ autoload.php
@@ -73,7 +73,7 @@ class Psr4AutoloaderClass {
 
 		// work backwards through the namespace names of the fully-qualified
 		// class name to find a mapped file name
-		while ( false !== $pos = strrpos( $prefix, '\\' ) ) { // phpcs:ignore WordPress.CodeAnalysis.AssignmentInCondition.FoundInWhileCondition
+		while ( false !== $pos = strrpos( $prefix, '\\' ) ) { // phpcs:ignore Generic.CodeAnalysis.AssignmentInCondition.FoundInWhileCondition
 
 			// retain the trailing namespace separator in the prefix
 			$prefix = substr( $class_name, 0, $pos + 1 );
